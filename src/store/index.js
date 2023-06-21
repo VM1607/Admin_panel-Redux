1597ad0b247b9734456cf4cd8ad9f3a31427374f
Vladislav Murzin <vlad99samara@mail.ru>
2023-06-21 21:00:24 +0400

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import heroes from '../components/heroesList/heroesSlice';
+import filters from '../components/heroesFilters/filtersSlice';
+import { apiSlice } from '../api/apiSlice';
+
+
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+};
+
+// const store = createStore( 
+//                 combineReducers({heroes, filters}),
+//                 compose(applyMiddleware(ReduxThunk, stringMiddleware), 
+//                     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+//                 );
+
+
+const store = configureStore({
+    reducer: {
+        filters, 
+        [apiSlice.reducerPath]: apiSlice.reducer},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware, apiSlice.middleware),
+    devTools: process.env.NODE_ENV !== 'production'
+})
+
+export default store;
